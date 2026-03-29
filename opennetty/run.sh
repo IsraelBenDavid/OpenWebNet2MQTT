1d65021a6067c8cb94bb44b6579fc470b586959c
Israel Ben David <55050010+IsraelBenDavid@users.noreply.github.com>
2026-03-29 15:01:40 +0300

    Remove Endpoint tag from Unit XML
    Remove the Endpoint tag from the Unit XML structure.

--- opennetty/run.sh
+++ opennetty/run.sh
@@ -160,7 +160,6 @@ if [ -f "$DEVICES_LIST_PATH" ]; then
 
             UNITS_XML="${UNITS_XML}
     <Unit Id=\"${UNIT_ID}\">
-      <Endpoint${EP_ATTR} />
     </Unit>"
             j=$((j + 1))
         done
